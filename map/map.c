69f49c20eca13f6073195d0c2574fc73d470d720
benwernicke <ben.wernicke@student.hpi.uni-potsdam.de>
2022-07-05 15:59:35 +0200

    map_iterator

--- map/map.c
+++ map/map.c
@@ -1,9 +1,10 @@
 #include "map.h"
 
+static ds_error_t check_realloc_(map_t* map);
 static uint64_t map_position_(map_t* map, void* key);
-static uint64_t stack_pop_(map_t* map);
-static void stack_push_buf_(map_t* map, uint64_t iter, uint64_t size);
-static void stack_push_(map_t* map, uint64_t i);
+static uint64_t stack_pop_(map_t* map, uint64_t* stack);
+static void stack_push_buf_(map_t* map, uint64_t* stack, uint64_t iter, uint64_t size);
+static void stack_push_(map_t* map, uint64_t* stack, uint64_t i);
 static uint64_t map_size_(map_t* map);
 
 typedef struct entry_t entry_t;
@@ -19,7 +20,8 @@ struct map_t {
     entry_t* buf;
     uint64_t* map;
 
-    uint64_t stack;
+    uint64_t unused_stack;
+    uint64_t used_stack;
 
     map_hash_function_t hash;
     map_cmp_function_t cmp;
@@ -37,7 +39,8 @@ ds_error_t map_create(map_t** map, map_hash_function_t hash, map_cmp_function_t 
         return BAD_ALLOC;
     }
     (*map)->buf_allocated = initial_size;
-    (*map)->stack = -1;
+    (*map)->unused_stack = -1;
+    (*map)->used_stack = -1;
     (*map)->hash = hash;
     (*map)->cmp = cmp;
 
@@ -50,7 +53,7 @@ ds_error_t map_create(map_t** map, map_hash_function_t hash, map_cmp_function_t 
         free(*map);
         return BAD_ALLOC;
     }
-    stack_push_buf_(*map, 0, (*map)->buf_allocated);
+    stack_push_buf_(*map, &(*map)->unused_stack, 0, (*map)->buf_allocated);
     return SUCCESS;
 }
 
@@ -65,23 +68,24 @@ ds_error_t map_free(map_t* map)
     return SUCCESS;
 }
 
-static void stack_push_(map_t* map, uint64_t i)
+static void stack_push_(map_t* map, uint64_t* stack, uint64_t i)
 {
-    map->buf[i].next = map->stack;
-    map->stack = i;
+    map->buf[i].next = *stack;
+    /*map->stack = i;*/
+    *stack = i;
 }
 
-static void stack_push_buf_(map_t* map, uint64_t iter, uint64_t size)
+static void stack_push_buf_(map_t* map, uint64_t* stack, uint64_t iter, uint64_t size)
 {
     for (; iter < size; iter++) {
-        stack_push_(map, iter);
+        stack_push_(map, stack, iter);
     }
 }
 
-static uint64_t stack_pop_(map_t* map)
+static uint64_t stack_pop_(map_t* map, uint64_t* stack)
 {
-    uint64_t r = map->stack;
-    map->stack = map->buf[map->stack].next;
+    uint64_t r = *stack;
+    *stack = map->buf[*stack].next;
     return r;
 }
 
@@ -96,7 +100,7 @@ static uint64_t map_position_(map_t* map, void* key)
 
 static ds_error_t check_realloc_(map_t* map)
 {
-    if (map->stack != -1) {
+    if (map->unused_stack != -1) {
         return SUCCESS;
     }
 
@@ -115,7 +119,7 @@ static ds_error_t check_realloc_(map_t* map)
     map->buf = new_buf;
     map->map = new_map;
     memset(map->map, 0xFF, map_size_(map) * sizeof(*map->map));
-    stack_push_buf_(map, old_allocated, map->buf_allocated);
+    stack_push_buf_(map, &map->unused_stack, old_allocated, map->buf_allocated);
 
     uint64_t i;
     for (i = 0; i < old_allocated; i++) {
@@ -134,7 +138,8 @@ ds_error_t map_insert(map_t* map, void* key, void* value)
     }
     uint64_t i = map_position_(map, key);
     if (map->map[i] == -1) {
-        uint64_t n = stack_pop_(map);
+        uint64_t n = stack_pop_(map, &map->unused_stack);
+        stack_push_(map, &map->used_stack, n);
         if (n == -1) {
             return BAD_ALLOC;
         }
@@ -162,8 +167,55 @@ ds_error_t map_delete(map_t* map, void* key)
     uint64_t i = map_position_(map, key);
     uint64_t n = map->map[i];
     if (n != -1) {
-        stack_push_(map, n);
+        stack_push_(map, &map->unused_stack, n);
         map->map[i] = -1;
     }
+
+    {
+        map_iterator_t prev = -1;
+        map_iterator_t iter;
+        map_iterator_t end;
+        map_iterator_begin(map, &iter);
+        map_iterator_end(map, &end);
+        for (; iter != end; map_iterator_next(map, &iter)) {
+            if (iter == i) {
+                if (prev == -1) {
+                    stack_pop_(map, &map->used_stack);
+                } else {
+                    map->buf[prev].next = map->buf[iter].next;
+                }
+            }
+            prev = iter;
+        }
+    }
+
+    return SUCCESS;
+}
+
+void* map_iterator_key(map_t* map, map_iterator_t iter)
+{
+    return map->buf[map->map[iter]].key;
+}
+
+void* map_iterator_value(map_t* map, map_iterator_t iter)
+{
+    return map->buf[map->map[iter]].value;
+}
+
+ds_error_t map_iterator_begin(map_t* map, map_iterator_t* iter)
+{
+    *iter = map->used_stack;
+    return SUCCESS;
+}
+
+ds_error_t map_iterator_next(map_t* map, map_iterator_t* iter)
+{
+    *iter = map->buf[*iter].next;
+    return SUCCESS;
+}
+
+ds_error_t map_iterator_end(map_t* map, map_iterator_t* end)
+{
+    *end = -1;
     return SUCCESS;
 }
--- map/map.h
+++ map/map.h
@@ -13,10 +13,18 @@ typedef struct map_t map_t;
 typedef uint64_t (*map_hash_function_t)(void* key);
 typedef bool (*map_cmp_function_t)(void* a, void* b);
 
+typedef uintptr_t map_iterator_t;
+
 ds_error_t map_get(map_t* map, void* key, void** value);
 ds_error_t map_insert(map_t* map, void* key, void* value);
 ds_error_t map_free(map_t* map);
 ds_error_t map_create(map_t** map, map_hash_function_t hash, map_cmp_function_t cmp, uint64_t initial_size);
 ds_error_t map_delete(map_t* map, void* key);
+
+ds_error_t map_iterator_begin(map_t* map, map_iterator_t* iter);
+ds_error_t map_iterator_end(map_t* map, map_iterator_t* end);
+void* map_iterator_key(map_t* map, map_iterator_t iter);
+void* map_iterator_value(map_t* map, map_iterator_t iter);
+ds_error_t map_iterator_next(map_t* map, map_iterator_t* iter);
 
 #endif
