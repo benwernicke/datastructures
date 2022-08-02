f496e31c5f16e4f9b25ba92c286fbb1ee9ec75f2
= <=>
2022-08-02 15:12:57 +0200

    panic actually panics

--- panic/panic.h
+++ panic/panic.h
@@ -1,16 +1,19 @@
 #ifndef PANIC_H
 #define PANIC_H
 
-#define panic(...)                                    \
-    {                                                 \
-        fprintf(stderr, "\033[31mError: \033[39m\n"); \
-        fprintf(stderr, __VA_ARGS__);                 \
-        fprintf(stderr, "\n");                        \
+#include <stdlib.h>
+
+#define panic(...)                                                             \
+    {                                                                          \
+        fprintf(stderr, "\033[31mError: \033[39m\n");                          \
+        fprintf(stderr, __VA_ARGS__);                                          \
+        fprintf(stderr, "\n");                                                 \
+        exit(1);                                                               \
     }
 
-#define panic_if(cond, ...) \
-    if (cond) {             \
-        panic(__VA_ARGS__); \
+#define panic_if(cond, ...)                                                    \
+    if (cond) {                                                                \
+        panic(__VA_ARGS__);                                                    \
     }
 
 #endif
